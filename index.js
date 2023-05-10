effffbba5b8c2bf110d2bfcf805113f748ed2143
Santiago Quintero <santiago.qp86@gmail.com>
2023-05-10 11:53:24 -0500

    3.8: Phonebook backend step8

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
 const express = require('express')
+const morgan = require('morgan')
 const app = express()
 
+morgan.token('dataPOST', function getData(req) {
+    return JSON.stringify(req.body)
+})
+
 app.use(express.json())
+app.use(morgan( ':method :url :status :res[content-length] - :response-time ms :dataPOST' ))
 
 let persons = [
     { 
@@ -86,6 +92,7 @@ app.post('/api/persons', (request, response) => {
 
     persons = persons.concat(person)
     response.json(persons)
+    //console.log(request.body)
 })
 
 const PORT = 3001
